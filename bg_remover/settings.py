058ada7e68e91b3f349e6972f9934685e61321c7
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-03-07 10:29:30 -0600

    Refactor Django settings for bg_remover project

--- bg_remover/settings.py
+++ bg_remover/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -22,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c++jms)_ta6c6(#c=u*1@od0&y9kuu*i6s-ft#)-%)_^(ju1x$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Set False in production
+DEBUG = True
 
-ALLOWED_HOSTS = ["clip-away-producer-bjcufthmg3gzdeaw.eastus2-01.azurewebsites.net"]
+ALLOWED_HOSTS = ["*]
+
 
 # Application definition
 
@@ -39,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",  # Must be first
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -68,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "bg_remover.wsgi.application"
 
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -77,6 +80,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -96,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -107,10 +112,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-# CORS configuration
-CORS_ALLOWED_ORIGINS = [
-    "https://clip-away.web.app",  # Only allow your frontend
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -118,15 +123,9 @@ CORS_ALLOW_HEADERS = [
     "content-type",
     "authorization",
     "x-csrftoken",
-    "x-requested-with",
 ]
 
-
-ALLOWED_HOSTS = [
-    "https://clip-away-producer-bjcufthmg3gzdeaw.eastus2-01.azurewebsites.net",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -136,4 +135,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
